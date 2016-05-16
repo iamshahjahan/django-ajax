269c9036744ddb2eb7577e3e31b80ecc0d229aa1
Khan <khan@khan.(none)>
2016-05-16 13:34:40 +0530

    Added a custom validator in models.py

--- ajax/models.py
+++ ajax/models.py
@@ -4,13 +4,29 @@ from django.db import models
 from django import forms
 
 from django.forms import ModelForm
+from django.core.exceptions import ValidationError
+from django.utils.translation import ugettext_lazy as _
+
+# from django.core.validators import validators
 
 # Create your models here.
 
+def validate_name(value):
+    if len(value) < 10:
+        raise ValidationError(
+            _('%(value)s is less than 10 characters.'),
+            params={'value': value},
+        )
+
+
+
+
+
+
 class Student(models.Model):
-	name = models.CharField(max_length = 20 )
+	name = models.CharField(max_length = 20,validators=[validate_name] )
 	password = models.CharField(max_length = 100 )
-	email = models.CharField( max_length = 10 )
+	email = models.EmailField( max_length = 50 )
 
 
 class StudentForm(forms.ModelForm):
